63385d817b37f59524111f1c4f7d61d8a7fe8254
Reginah24 <r.majura@alustudent.com>
2025-03-04 17:53:11 +0200

    tired

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(10):
     for j in range(10):
         if i < j:
-            print("{:02d}, {:02d}".format(i, j), end="")
+            print("{:02d}".format(i) + ", " + "{:02d}".format(j), end="")
 print()
