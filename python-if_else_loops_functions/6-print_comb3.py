ef05d220f6ee23f94152a1dfede97f0dfe314bed
Reginah24 <r.majura@alustudent.com>
2025-03-04 17:50:39 +0200

    tired

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
-            break
-        print("{}{}".format(i, j), end=", ")
+for i in range(10):
+    for j in range(10):
+        if i < j:
+            print("{:02d}, {:02d}".format(i, j), end="")
+print()
