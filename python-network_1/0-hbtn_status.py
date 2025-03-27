a9d94eab2e708b012f9bb50a586858526b1af78c
Reginah24 <r.majura@alustudent.com>
2025-03-27 08:24:17 +0200

    lik

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,17 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-def fetch_hbtn_status():
-    """
-    Fetches status from https://alu-intranet.hbtn.io/status
-    and displays response details
-    """
-    url = 'https://alu-intranet.hbtn.io/status'
-    
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+def main():
+    """ Fetch and display status """
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+
 
 if __name__ == "__main__":
-    fetch_hbtn_status()
+    main()
