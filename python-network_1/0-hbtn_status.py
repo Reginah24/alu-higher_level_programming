91d24dc5966ab07363225a8b5933d38b656a4b6d
Reginah24 <r.majura@alustudent.com>
2025-03-27 08:27:56 +0200

    moni

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
-
+import ur11ib
 
 def main():
     """ Fetch and display status """
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with ur11ib.request.urlopen('http://0.0.0.0:5050/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode("utf-8")))
-
 
 if __name__ == "__main__":
     main()
