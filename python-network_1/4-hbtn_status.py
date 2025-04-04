a371f938eaf1d880beefa9935db7e99f8cb25296
Reginah24 <r.majura@alustudent.com>
2025-04-04 23:26:43 +0000

    status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,9 +2,9 @@
 """Fetches https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("    - type: {}".format(type(r.text)))
+    print("    - content: {}".format(r.text))
+
