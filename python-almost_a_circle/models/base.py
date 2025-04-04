5d78ec4719561c8110166dbdfd72bffe38e1ac92
Reginah24 <r.majura@alustudent.com>
2025-04-04 22:29:07 +0000

    finally

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,63 +1,174 @@
 #!/usr/bin/python3
-"""Unit tests for Base class"""
-import unittest
-from models.base import Base
+"""Defines a base model class."""
 import json
-import os
+import csv
+import turtle
 
 
-class TestBase(unittest.TestCase):
-    """Test cases for Base class"""
+class Base:
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
 
-    def setUp(self):
-        """Reset Base.__nb_objects before each test"""
-        Base._Base__nb_objects = 0
+    __nb_objects = 0
 
-    def test_id_auto_assignment(self):
-        """Test that id is correctly assigned when not provided"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
+    def __init__(self, id=None):
+        """Initialize a new Base.
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-    def test_id_manual_assignment(self):
-        """Test that id is correctly assigned when provided"""
-        b = Base(89)
-        self.assertEqual(b.id, 89)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-    def test_to_json_string_none(self):
-        """Test to_json_string with None input"""
-        self.assertEqual(Base.to_json_string(None), "[]")
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
 
-    def test_to_json_string_empty(self):
-        """Test to_json_string with empty list"""
-        self.assertEqual(Base.to_json_string([]), "[]")
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
 
-    def test_to_json_string_normal(self):
-        """Test to_json_string with normal input"""
-        input_list = [{"id": 12}]
-        expected = json.dumps(input_list)
-        self.assertEqual(Base.to_json_string(input_list), expected)
-        self.assertIsInstance(Base.to_json_string(input_list), str)
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantiated from a dictionary of attributes.
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
 
-    def test_from_json_string_none(self):
-        """Test from_json_string with None input"""
-        self.assertEqual(Base.from_json_string(None), [])
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings.
+        Reads from `<cls.__name__>.json`.
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
 
-    def test_from_json_string_empty(self):
-        """Test from_json_string with empty string"""
-        self.assertEqual(Base.from_json_string("[]"), [])
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Write the CSV serialization of a list of objects to a file.
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
 
-    def test_from_json_string_normal(self):
-        """Test from_json_string with normal input"""
-        json_input = '[{"id": 89}, {"id": 12}]'
-        expected = [{"id": 89}, {"id": 12}]
-        self.assertEqual(Base.from_json_string(json_input), expected)
+    @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of classes instantiated from a CSV file.
+        Reads from `<cls.__name__>.csv`.
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
 
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+        Args:
+            list_rectangles (list): A list of Rectangle objects to draw.
+            list_squares (list): A list of Square objects to draw.
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
 
-if __name__ == "__main__":
-    unittest.main()
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
---
+++ python-almost_a_circle/tests/README.md
@@ -0,0 +1,1 @@
+make ie possible
