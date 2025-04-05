e27b246ff39977c923b5eb6dfc41a76239a35c6e
Reginah24 <r.majura@alustudent.com>
2025-04-05 06:49:34 +0000

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ import turtle
 
 class Base:
     """Represent the base model.
-    Represents the "base" for all other classes in project 0x0C*.
+    Represents the "base" for all other classes in this project
     Attributes:
         __nb_objects (int): The number of instantiated Bases.
     """
